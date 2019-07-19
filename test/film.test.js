8460fae279fde247d74580c4ee49a756ed4f795a
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-19 11:03:15 -0700

    Create test for get film by ID using /GET

--- test/film.test.js
+++ test/film.test.js
@@ -4,9 +4,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+
 const Film = require('../lib/models/Film');
 const Actor = require('../lib/models/Actor');
 const Studio = require('../lib/models/Studio');
+const Review = require('../lib/models/Review');
+const Reviewer = require('../lib/models/Reviewer');
 
 describe('test film routes', () => {
 
@@ -20,10 +23,16 @@ describe('test film routes', () => {
 
   let actor = null;
   let studio = null;
+  let reviewer = null;
+  let review = null;
+  let film = null;
 
   beforeEach(async() => {
     actor = JSON.parse(JSON.stringify(await Actor.create({ name: 'Danny' })));
     studio = JSON.parse(JSON.stringify(await Studio.create({ name: 'Danny' })));
+    reviewer = JSON.parse(JSON.stringify(await Reviewer.create({ name: 'Danny', company: 'Fisher' })));
+    film = JSON.parse(JSON.stringify(await Film.create({ title: 'Aladdin', studio: studio._id, released: 1997, cast: [{ role: 'Lead', actor: actor._id }] })));
+    review = JSON.parse(JSON.stringify(await Review.create({ rating: 3, reviewer: reviewer._id, review: 'meh', film: film._id })));
   });
 
   afterAll(() => {
@@ -57,46 +66,28 @@ describe('test film routes', () => {
   });
 
   it('test get all films using /GET', async() => {
-    Film.create({
-      title: 'Up',
-      studio: studio._id,
-      released: 1997,
-      cast: [{
-        actor: actor._id
-      }]
-    });
 
     return request(app)
       .get('/api/v1/films')
       .then(res => {
         expect(res.body).toEqual([
           {
-            _id: expect.any(String),
-            title: 'Up',
+            _id: film._id,
+            title: film.title,
             studio: { _id: studio._id, name: studio.name },
-            released: 1997
+            released: film.released
           }
         ]);
       });
   });
 
   it('can get a film by its ID using GET', async() => {
-    const film = await Film.create({
-      title: 'Up',
-      studio: studio._id,
-      released: 1997,
-      cast: [{
-        role: 'Lead',
-        actor: actor._id
-      }]
-    });
 
     return request(app)
       .get(`/api/v1/films/${film._id}`)
       .then(res => {
         expect(res.body).toEqual({
-          _id: expect.any(String),
-          title: 'Up',
+          title: film.title,
           released: 1997,
           studio: {
             _id: studio._id,
@@ -107,6 +98,15 @@ describe('test film routes', () => {
             role: expect.any(String),
             actor: { _id: actor._id, name: actor.name }
           }],
+          reviews: [{
+            _id: expect.any(String),
+            rating: review.rating,
+            review: review.review,
+            reviewer: {
+              _id: reviewer._id,
+              name: reviewer.name
+            }
+          }]
         });
       });
   });
